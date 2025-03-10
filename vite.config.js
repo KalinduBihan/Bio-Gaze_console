91c54aa7f2fa6af127e40f7aadc4facc698339d3
KalinduBihan <klindubihanfdo@gmail.com>
2025-03-10 08:57:52 +0530

    commit - 03

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures relative paths for assets in production
-  server: {
-    port: 3000, // Ensures local development runs on a known port
-  },
 });
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
